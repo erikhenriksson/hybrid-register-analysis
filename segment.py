e8e15f88c24d431fc8be0cc16ae895ba861e3884
Erik <eekihenriksson@gmail.com>
2024-12-28 15:38:42 +0200

    Update segment.py

--- segment.py
+++ segment.py
@@ -147,7 +147,7 @@ model = AutoModelForSequenceClassification.from_pretrained(model_name)
 tokenizer = AutoTokenizer.from_pretrained(tokenizer_name)
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 max_tokens = 512
-min_words = 50
+min_words = 20
 max_segments = 20
 model = model.to(device)
 model.eval()
